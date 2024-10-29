eae8da57abb5c8883445af5bee0bf19236e6b644
akiraacs <alfabeto.coradini@hotmail.com>
2024-10-29 01:19:23 -0300

    Adiciona metodos para obter todas as tasks e para adicionar tasks

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -1,2 +1,16 @@
 package repository
 
+import "github.com/akiraacs/go-todolist-rest-api/internal/models"
+
+
+type Task struct {
+	tasks []models.Task
+}
+
+func (t *Task) GetAllTasks() []models.Task {
+	return t.tasks
+}
+
+func (t *Task) AddTask(task models.Task) {
+	t.tasks = append(t.tasks, task)
+}
