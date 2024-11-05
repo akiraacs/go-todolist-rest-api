c85742aaed09a5f82fc93cabeacbde56360bd98f
akiraacs <alfabeto.coradini@hotmail.com>
2024-11-04 23:45:43 -0300

    Adiciona tratativa para obter tasks especificas a partir dos parametros da requisicao

--- monolito/main.go
+++ monolito/main.go
@@ -16,17 +16,18 @@ const (
 	StatusCompleted  taskStatus = "Completed"
 )
 
-type task struct {
+type Task struct {
 	ID          uint       `json:"id"`
 	Title       string     `json:"title"`
 	Status      taskStatus `json:"status"`
 	Description string     `json:"description"`
 }
 
-var tasks = []task{
+var tasks = []Task{
 	{ID: 1, Title: "Lavar louça", Status: StatusCompleted, Description: "Necessário lavar louça todos os dias"},
 	{ID: 2, Title: "Tirar lixo", Status: StatusPending, Description: "Descer com as sacolas de lixo"},
 	{ID: 3, Title: "Passar pano", Status: StatusInProgress, Description: "Passar pano e deixar a casa limpa"},
+	{ID: 4, Title: "Estudar", Status: StatusInProgress, Description: "Estudar pra ficar rico né pae"},
 }
 
 func main() {
@@ -34,16 +35,36 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/tasks", getAllTasks)
+	router.GET("/tasks", getTasks)
 	router.GET("/tasks/:id", getTaskByID)
 
 	router.Run(":8080")
-
 }
 
 // getAllTasks retorna todas as tasks cadastradas
-func getAllTasks(c *gin.Context) {
+func getTasks(c *gin.Context) {
+	specificTasks := getSpecificTasks(c)
+
+	if len(specificTasks) != 0 {
+		tasks = specificTasks
+	}
+
 	c.IndentedJSON(http.StatusOK, tasks)
+}
+
+func getSpecificTasks(c *gin.Context) []Task {
+	var specificTasks []Task
+
+	title := c.Query("title")
+	status := c.Query("status")
+
+	// Obtem as tasks correspondentes aos parâmetros da requisição
+	for _, task := range tasks {
+		if (title == "" || task.Title == title) && (status == "" || string(task.Status) == status) {
+			specificTasks = append(specificTasks, task)
+		}
+	}
+	return specificTasks
 }
 
 // getTaskByID retorna uma task especifica de acordo com o ID da task
